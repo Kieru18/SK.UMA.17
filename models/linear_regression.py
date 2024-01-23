bdee9fa6dcde6eb462c2b0b7460c1fc0eb4a8e4c
kziarek <01169264@pw.edu.pl>
2024-01-23 21:54:02 +0100

    Working linear regression

--- models/linear_regression.py
+++ models/linear_regression.py
@@ -1,78 +1,78 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import CONST
-
-np.random.seed = CONST.SEED
-
+import gc
 
 class CustomLinearRegression:
-    def __init__(self, learning_rate=0.01, num_iterations=1000, cost_function='mse', learning_function='gradient_descent'):
+    def __init__(self, learning_rate=0.0002, num_iterations=10000, cost_function='mse', learning_function='gradient_descent'):
         self.learning_rate = learning_rate
         self.num_iterations = num_iterations
         self.cost_function = cost_function
         self.learning_function = learning_function
+        self.y = None
+        self.y_pred = None
+        self.X_b = None
         self.theta = None  # Parametry modelu
 
     def fit(self, X, y):
         # Dodanie kolumny jednostkowej do zbioru danych trenujących z wartością stałą (bias)
-        X_b = np.c_[np.ones((X.shape[0], 1)), X]
+        X = np.asarray(X)
+        y = np.asarray(y).reshape(-1, 1)
+        
+        self.X_b = np.c_[np.ones((X.shape[0], 1)), X]
+        self.y = y
 
         # Inicjalizacja parametrów theta
-        self.theta = np.random.randn(X_b.shape[1], 1)
-
+        self.theta = np.ones(self.X_b.shape[1]).reshape(-1, 1)
+  
+     
         for iteration in range(self.num_iterations):
-            # Obliczenie predykcji
-            y_pred = X_b.dot(self.theta)
+            # Obliczenie predykcji Macierz wektorów atrybutów * wagi
+            self.y_pred = self.X_b.dot(self.theta)
+            print(self.y_pred.shape)
 
-            # Obliczenie gradientu funkcji kosztu
-            gradient = self.calculate_gradient(X_b, y, y_pred)
-
-            print(f'theta: {self.theta}')
-            print(f'theta_shape: {np.shape(self.theta)}')
-            
-            # Aktualizacja parametrów theta w zależności od metody optymalizacji
-            if self.learning_function == 'gradient_descent':
+            if self.learning_function == 'gradient_descent' and self.cost_function == 'mse':
+                gradient = self.base_gradient_mse()
                 self.theta -= self.learning_rate * gradient
-            
-            # elif self.learning_function == 'stochastic_gradient_descent':
-            #     random_index = np.random.randint(X_b.shape[0])
-            #     xi = X_b[random_index:random_index+1]
-            #     yi = y[random_index:random_index+1]
-            #     gradient = self.calculate_gradient(xi, yi, xi.dot(self.theta))
-            #     self.theta -= self.learning_rate * gradient
-            else:
-                raise ValueError("Nieznana metoda optymalizacji.")
+                print(self.theta)
+   
+            if self.learning_function == 'stohastic_gradient_descent' and self.cost_function == 'mse':
+                indices = list(range(X.shape[0]))
+                np.random.shuffle(indices)
 
-    def calculate_gradient(self, X, y, y_pred):
-        # Obliczenie gradientu funkcji kosztu w zależności od funkcji kosztu
-        if self.cost_function == 'mse':
-            print(f'XT {X.T}')
-            gradient = 2/X.shape[0] * X.T.dot(y_pred - y)
-            print(f'gradient: {gradient}')
-            return gradient
-        else:
-            raise ValueError("Nieznana funckja kosztu.")
-        
+                for idx in indices:
+                    x_sample = self.X_b[idx:idx+1]
+                    y_sample = self.y[idx:idx+1]
+
+                    self.y_pred = x_sample.dot(self.theta)
+                    gradient = self.base_gradient_mse(x_sample, y_sample)
+                    self.theta -= self.learning_rate * gradient
+
+
+    def base_gradient_mse(self):
+        gradient = self.X_b.T.dot(2*(self.y_pred - self.y)) / self.X_b.shape[0]
+        return gradient
+
+
+    def calculate_mse(self, X, y, y_pred):
+        m = X.shape[0]
+        mse = ((1/m)) * np.sum(np.power(y-y_pred, 2))
+        return mse
+
+    def calculate_mae(self, X, y, y_pred):
+        m = X.shape[0]
+        mae = (1/m) * np.sum(np.abs(y-y_pred))
+        return mae
+
+    def calculate_huber_loss(self, X, y, y_pred, delta=1.0):
+        absolute_errors = np.abs(y - y_pred)
+        square_errors = 0.5 * (absolute_errors ** 2)
+        linear_errors = delta * (absolute_errors - 0.5 * delta)
+
+        loss = np.where(absolute_errors <= delta, square_errors, linear_errors)
+        return np.mean(loss)
 
     def predict(self, X):
-        # Dodanie kolumny jednostkowej do X (bias)
         X_b = np.c_[np.ones((X.shape[0], 1)), X]
         return X_b.dot(self.theta)
 
 
-X = 2 * np.random.rand(10, 4)
-y = 4 + 3 * X + np.random.randn(10, 1)
-
-# model = CustomLinearRegression(learning_rate=0.01, num_iterations=1000, cost_function='mse', learning_function='gradient_descent')
-# model.fit(X, y)
-
-
-X = np.asarray([[2, 3, 4], [3, 3, 1], [1, 1, 1]])
-y_pred = np.asarray([1, 2, 3])
-y = np.asarray([1, 1, 1])
-theta = np.asarray([1, 1, 3])
-
-dot = X.dot(theta)
-
-print(y_pred - y)
-print(X.T)
